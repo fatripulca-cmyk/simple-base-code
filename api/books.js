a087c9b1f63f53055bc3705e4dee954660398c65
tony-tripulca <tony.tripulca@gmail.com>
2025-12-18 17:39:42 +0800

    [feat] GET and POST generics, body-parser

--- api/books.js
+++ api/books.js
@@ -1,16 +1,34 @@
+const bodyParser = require('../lib/body-parser');
+
 const books = [
   { id: 1, title: 'Clean Code' },
   { id: 2, title: 'You Don\'t Know JS' }
 ];
 
-module.exports = function bookService(req, res) {
-  if (req.method !== 'GET') {
-    res.statusCode = 405;
-    res.setHeader('Allow', 'GET');
-    res.end('Method Not Allowed');
-    return;
+module.exports = async function bookService(req, res) {
+  switch (req.method) {
+    // GET /api/books
+    case 'GET':
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(books));
+      return;
+    // POST /api/books
+    case 'POST':
+      try {
+        const data = await bodyParser(req);
+        books.push(data);
+        res.statusCode = 201;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data));
+      } catch (err) {
+        res.statusCode = 400;
+        res.end(err.message);
+      }
+      return;
+    default:
+      res.statusCode = 405;
+      res.setHeader('Allow', 'GET, POST');
+      res.end('Method Not Allowed');
+      return;
   }
-
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(books));
 };
--- api/users.js
+++ api/users.js
@@ -1,16 +1,34 @@
-const users = [
+const bodyParser = require('../lib/body-parser');
+
+let users = [
   { id: 1, name: 'Alice' },
   { id: 2, name: 'Bob' }
 ];
 
-module.exports = function userService(req, res) {
-  if (req.method !== 'GET') {
-    res.statusCode = 405;
-    res.setHeader('Allow', 'GET');
-    res.end('Method Not Allowed');
-    return;
+module.exports = async function userService(req, res) {
+  switch (req.method) {
+    // GET /api/users
+    case 'GET':
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(users));
+      return;
+    // POST /api/users
+    case 'POST':
+      try {
+        const data = await bodyParser(req);
+        users.push(data);
+        res.statusCode = 201;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data));
+      } catch (err) {
+        res.statusCode = 400;
+        res.end(err.message);
+      }
+      return;
+    default:
+      res.statusCode = 405;
+      res.setHeader('Allow', 'GET, POST');
+      res.end('Method Not Allowed');
+      return;
   }
-
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(users));
 };
